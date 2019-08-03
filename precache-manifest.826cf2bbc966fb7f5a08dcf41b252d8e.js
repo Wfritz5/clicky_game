13f232580fe2649afffe7328d724a0ec4dec61f2
Will <wfritz5@gmail.com>
2019-08-03 10:26:04 -0600

    Updates

---
+++ precache-manifest.826cf2bbc966fb7f5a08dcf41b252d8e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0407b5a54ddacdbe3a33e901694b6079",
+    "url": "/clicky_game/index.html"
+  },
+  {
+    "revision": "b3a0afac8010df65f904",
+    "url": "/clicky_game/static/css/main.4b140465.chunk.css"
+  },
+  {
+    "revision": "58f496bc1ce27350f0eb",
+    "url": "/clicky_game/static/js/2.98dcc9dd.chunk.js"
+  },
+  {
+    "revision": "b3a0afac8010df65f904",
+    "url": "/clicky_game/static/js/main.6fb83cc0.chunk.js"
+  },
+  {
+    "revision": "ef6ad64b0df3315f3b6a",
+    "url": "/clicky_game/static/js/runtime~main.33627756.js"
+  },
+  {
+    "revision": "6943d69121283f62698327421dab943e",
+    "url": "/clicky_game/static/media/strawberry.6943d691.jpg"
+  }
+]);
